b36516099eb11aab203daebee8f89182c4faee65
Wesley Au <wesley.au.2020@scis.smu.edu.sg>
2023-07-24 16:54:13 +0800

    Add cross origins

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
